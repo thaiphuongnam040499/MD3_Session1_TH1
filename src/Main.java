9ffec35185b4ff93cf207ab2bd302cf2d75d94c0
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-07 08:56:44 +0700

    th1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello anh hiếu ngốc!");
+        Date date = new Date();
+        System.out.println("time out"+ date);
+    }
+}
